9815afbf4004ba4dfa8c2aeb5ee506dbd110ea0e
braceskabane <allzoldyck31@gmail.com>
2026-07-17 23:33:55 +0700

    fix: move base url backend to env

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-dev-ap4n.onrender.com/api/v1/",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, //browser mengizinkan pengiriman dan penerimaan cookie secara otomatis untuk refresh token.
 });
 
